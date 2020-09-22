62e9c09d4a755708dc686332cb02550e256d2382
Ana Nieto <40575584+cadirneca@users.noreply.github.com>
2020-09-22 21:09:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='judashive',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/cadirneca/judas',
+    license='',
+    author='Ana Nieto',
+    author_email='',
+    description='JUDAS process files in order to identify relationships between users and devices. This solution depends on the format of the files, so please take a look and modify what you need to process your files.'
+)
